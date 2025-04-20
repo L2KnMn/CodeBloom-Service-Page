ae7703d38820a63529ffb9470d7897e5bf7c47c1
2KnMn <rhksals7864@naver.com>
2025-04-20 09:00:27 +0900

    상세 보기 구현

--- product/views.py
+++ product/views.py
@@ -7,3 +7,9 @@ def index(request):
     content_list = MainContent.objects.order_by('-pub_date')
     context = {'content_list': content_list}
     return render(request, 'product/content_list.html', context)
+
+
+def detail(request, content_id):
+    content_list = MainContent.objects.get(id=content_id)
+    context = {'content_list': content_list}
+    return render(request, 'product/detail.html', context)
